339b0245ea604b4b97c24f09515ab5fc78f14d03
Carl Nordgren <cnordgre@andrew.cmu.edu>
2015-10-20 19:02:14 -0400

    Added address decoder for 6502

--- demo/top.sv
+++ demo/top.sv
@@ -5,21 +5,26 @@ module top();
    logic 	rst_l; 
    logic 	clock_15;
    logic 	sndirq, sndnmi;
+   logic 	audio_out;
+   logic 	pokey_sel;
+   logic 	RW_l, WR_l;
 
+   assign WR_l = ~RW_l;
+   
    //Block RAM for system memory
-
+   
    
    //Block ROM for program memory
-
+   
    
    //6502 Processor Instantiation
-
+   
    
    //Pokey Audio Synthesis Chip
-
+   
    
    //PWM Audio output interface
-
+   
    
    
 endmodule: top

a5c4a701c635cad5256c0cc4e0ab7a63849c3349
Carl Nordgren <cnordgre@andrew.cmu.edu>
2015-10-20 15:11:52 -0400

    Added midsemester demo subfolder and skeleton top module for demo.

---
+++ demo/top.sv
@@ -0,0 +1,25 @@
+
+module top();
+   logic [7:0] data;
+   logic [15:0] addr;
+   logic 	rst_l; 
+   logic 	clock_15;
+   logic 	sndirq, sndnmi;
+
+   //Block RAM for system memory
+
+   
+   //Block ROM for program memory
+
+   
+   //6502 Processor Instantiation
+
+   
+   //Pokey Audio Synthesis Chip
+
+   
+   //PWM Audio output interface
+
+   
+   
+endmodule: top
